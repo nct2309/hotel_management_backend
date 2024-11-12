3bc2a9e5456b551c3ea9a492e9afa979bb7d3c9c
nct2309 <nctstuti@gmail.com>
2024-11-13 03:48:17 +0700

    fix: recycle db connection pool

--- backend/src/app/core/db/database.py
+++ backend/src/app/core/db/database.py
@@ -12,7 +12,7 @@ class Base(DeclarativeBase, MappedAsDataclass):
 DATABASE_URI = settings.POSTGRES_URI
 DATABASE_URL = f"{DATABASE_URI}"
 
-async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+async_engine = create_async_engine(DATABASE_URL, echo=False, future=True, pool_recycle=3600)
 
 local_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
